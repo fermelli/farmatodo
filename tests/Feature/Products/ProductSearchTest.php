ba4d6c8e47d68137dcdcbaea5c3943edc6315de2
fermelli <luisfernandosalgadomiguez@gmail.com>
2022-06-02 23:40:23 -0400

    AÃnadir categoria a los productos

---
+++ tests/Feature/Products/ProductSearchTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature\Products;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductSearchTest extends TestCase
+{
+    use RefreshDatabase;
+}
